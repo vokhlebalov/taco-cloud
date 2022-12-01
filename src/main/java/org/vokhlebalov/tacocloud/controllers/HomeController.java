7c054fd7787e6d1d682414978736793ee30bbd71
Хлебалов Виталий Олегович <v.khlebalov@bftcom.com>
2022-12-01 23:02:01 +0500

    Глава 1 создал начальную страницу приложения

---
+++ src/main/java/org/vokhlebalov/tacocloud/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package org.vokhlebalov.tacocloud.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+}
