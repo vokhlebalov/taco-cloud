6b520fc82360a7934751b3b0e2720b744daa3b4a
Хлебалов Виталий Олегович <v.khlebalov@bftcom.com>
2022-12-30 23:31:52 +0500

    Глава 2.4-5 Добавил веб-конфигурацию для создания стартового контроллера представлений, добавил стили для страниц, настроил автоматическое обновление шаблонов

--- src/test/java/org/vokhlebalov/tacocloud/HomeControllerTest.java
+++ src/test/java/org/vokhlebalov/tacocloud/HomeControllerTest.java
@@ -4,13 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.vokhlebalov.tacocloud.controllers.HomeController;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(HomeController.class)
+@WebMvcTest
 public class HomeControllerTest {
 
     @Autowired
